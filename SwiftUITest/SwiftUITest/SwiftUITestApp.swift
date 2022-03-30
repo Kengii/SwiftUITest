be815e3df4e30fa30218fef26503b23d56b68a80
Kengii <97613101+Kengii@users.noreply.github.com>
2022-03-30 18:54:51 +0300

    Project Created

---
+++ SwiftUITest/SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by Владимир Данилович on 30.03.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
